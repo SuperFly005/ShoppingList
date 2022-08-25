ae1c78aa632740b48aaa862925f6283eae6a6f20
SuperFly005 <superfly.soft@gmail.com>
2022-08-25 09:56:25 -0400

    fix: < Item Dialog > animation & background

--- src/components/ShoppingList/ItemDialog.js
+++ src/components/ShoppingList/ItemDialog.js
@@ -1,27 +1,34 @@
 import React from 'react'
-import { CustomButton, CustomDropDown, CustomInput } from './Dialog';
-import "./ItemDialog.scss";
+import { CustomButton, CustomDropDown, CustomInput } from './Dialog'
+import './ItemDialog.scss'
 
-const ItemDialog = ({ closeDialog }) => {
+const ItemDialog = ({ closeDialog, visible, type }) => {
 
   return (
-    <div className="item-dialog">
-      <div className='dialog-title'>
+    <div className="item-dialog" style={{ right: visible ? '0px' : '-560px' }}>
+      <div className="dialog-title">
         SHOPPING LIST
-        <div className="material-icons" onClick={closeDialog}>chevron_right</div>
+        <div className="material-icons" onClick={closeDialog}>
+          chevron_right
+        </div>
       </div>
-      <div className='dialog-content'>
-        <div className='dialog-content-tag'>Add an Item</div>
-        <div className='dialog-content-subtag'>Add your new item below</div>
-        <CustomInput placeHolder="Item Name" singleType/>
-        <CustomInput placeHolder="Description" singleType={false}/>
-        <CustomDropDown options={[1, 2, 3]}/>
+      <div className="dialog-content">
+        <div className="dialog-content-tag">{type === "new" ? "Add an Item" : "Edit an Item"}</div>
+        <div className="dialog-content-subtag">{type === "new" ? "Add your new item below" : "Edit your item below"}</div>
+        <CustomInput placeHolder="Item Name" singleType />
+        <CustomInput placeHolder="Description" singleType={false} />
+        <CustomDropDown options={[1, 2, 3]} />
       </div>
-      <div className='dialog-button-group'>
-          <CustomButton value="Cancel" type="delete" style={{marginRight: '30px'}} clickHandler={closeDialog}/>
-          <CustomButton value="Add Task" type="active"/>
+      <div className="dialog-button-group">
+        <CustomButton
+          value="Cancel"
+          type="delete"
+          style={{ marginRight: '30px' }}
+          clickHandler={closeDialog}
+        />
+        <CustomButton value={type === "new" ? "Add Task" : "Save Item"} type="active" />
       </div>
-      <div className='dialog-footer'/>
+      <div className="dialog-footer" />
     </div>
   )
 }
--- src/pages/ShoppingList.js
+++ src/pages/ShoppingList.js
@@ -7,15 +7,16 @@ import {
 import './ShoppingList.scss'
 
 const ShoppingList = () => {
-  const [dialog, setDialog] = useState(false)
+  const [dialogState, setDialogState] = useState(false)
 
   return (
     <div className="shopping-list">
       <ShoppingListHeader />
       <div className="shopping-list-container">
-        <EmptyList showDialog={() => setDialog(true)} />
+        <EmptyList showDialog={() => setDialogState(true)} />
       </div>
-      {dialog && <ItemDialog closeDialog={() => setDialog(false)}/>}
+      <ItemDialog closeDialog={() => setDialogState(false)} visible={dialogState} type="new"/>
+      {dialogState && <div className='dialog-back' onClick={() => setDialogState(false)}></div>}
     </div>
   )
 }
